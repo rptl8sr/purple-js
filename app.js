47dd84eb4dccb90e1b4879b799a55a3c575797e8
rptl8sr <rptl8sr@gmail.com>
2023-05-24 00:31:40 +0300

    5-languages

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+// languuages
+
+const preferredLanguage = prompt('Enter your preferred language (fr, en, ru)', 'ru');
+
+switch (preferredLanguage) {
+  case 'fr':
+    alert('Bonjour');
+    break;
+  case 'en':
+    alert('Hello');
+    break;
+  case 'ru':
+    alert('Привет');
+    break;  
+  default:
+    alert('Sorry we do not speak this language')
+};
