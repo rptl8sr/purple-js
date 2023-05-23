ec7a6320d271c0e59824df26c4710a74d927b2b0
rptl8sr <rptl8sr@gmail.com>
2023-05-24 00:22:16 +0300

    4-navigator

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+// navigator
+
+const addressLat = prompt('Enter address latitude');
+const addressLon = prompt('Enter address longitude');
+const positionLat = prompt('Enter position latitude');
+const positionLon = prompt('Enter position longitude');
+
+const distance = Math.sqrt(
+  Math.pow(addressLat - positionLat, 2) +
+  Math.pow(addressLon - positionLon, 2)
+);
+
+alert(`Distance is ${distance} km.`);
