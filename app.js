3045248eaf1283b9034706d78861202bf8d98f87
rptl8sr <rptl8sr@gmail.com>
2023-05-24 14:00:11 +0300

    6-driver

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('hello world');
+const hasLicense = confirm('Do you have a license?');
+const age = prompt('How old are you?');
+const isDrunk = confirm('Are you drunk?');
+
+hasLicense && age >= 18 && !isDrunk ? alert('You can drive!') : alert('You can\'t drive!');
