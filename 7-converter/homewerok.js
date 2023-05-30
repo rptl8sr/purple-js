130157583c94fc5fefe2533ecfff9f145ecd251f
rptl8sr <rptl8sr@gmail.com>
2023-05-30 10:52:57 +0300

    remove unnecessary conversion to integer

--- 7-converter/homewerok.js
+++ 7-converter/homewerok.js
@@ -13,8 +13,6 @@ function currencyConverter(amount, from='rub', to='usd') {
 
 
 function getRandomInt(min=0, max=100) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
