18936b80bec7e39ae576ac0d82d4285d2a2e6ea2
rptl8sr <rptl8sr@gmail.com>
2023-06-01 01:55:57 +0300

    15-task-object

--- 13-query/homework.js
+++ 13-query/homework.js
@@ -1,5 +1,6 @@
 function splitQuery(query) {
-  return Object.entries(query).reduce((acc, [key, value]) => acc ? `${acc}&${key}=${value}` : `${key}=${value}`, '')
+  // return Object.entries(query).reduce((acc, [key, value]) => acc ? `${acc}&${key}=${value}` : `${key}=${value}`, '')
+  return Object.entries(query).map(([key, value]) => `${key}=${value}`).join('&'); // There's less calls and creating new variables 
 }
 
 const query = {
